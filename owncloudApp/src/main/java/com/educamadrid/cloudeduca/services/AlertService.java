7e3772e7d5ddf610684b635f4585d1a38a4f44b8
Rodrigo García López <rglopez300@gmail.com>
2022-09-12 10:17:57 +0200

    Añadido JSON de pruebas.

--- owncloudApp/src/main/java/com/educamadrid/cloudeduca/services/AlertService.java
+++ owncloudApp/src/main/java/com/educamadrid/cloudeduca/services/AlertService.java
@@ -11,9 +11,9 @@ import retrofit2.http.GET;
  * interface AlertService
  */
 public interface AlertService {
-    //@GET("public/news/js/apps.json")
+//    @GET("public/news/js/apps.json")
 //    @GET("b/SC0E") // Pruebas
-//    @GET("6ee42b4496a073aeb0e7") // con obj cloud
+    @GET("6ee42b4496a073aeb0e7") // con obj cloud
 //    @GET("a47706d1beedd6bbaad4") // sin obj cloud
     Call<AlertObject> getAlert();
 }
